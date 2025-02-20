d8f5a15701eb146e3adc254d6f299894ec4c6bc6
Vishal Prajapati <127581301+vishalprajapati2003@users.noreply.github.com>
2025-02-20 15:49:50 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Body from "./components/Body";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <div>
+      <Body />
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
